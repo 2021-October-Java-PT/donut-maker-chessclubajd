2087fbb8f18b309648120ebe6850738ac728eeeb
chessclubajd <chessclubajd@yahoo.com>
2022-01-31 18:42:34 -0500

    2nd try

--- donut-maker-app.js
+++ donut-maker-app.js
@@ -7,7 +7,7 @@ function renderPage() {
   addAutoClicker();
 }
 
-const donutMaker = new DonutMaker(0, 0, 100, 10, 0);
+const donutMaker = new DonutMaker(0, 0, 10, 10, 0);
 const donutTallyDisplay = document.querySelector("#donutCntr");
 
 function addADonut() {
@@ -18,7 +18,6 @@ function addADonut() {
     donutTallyDisplay.innerText = donutMaker.donutTally;
     console.log(donutMaker.donutTally);
   });
-
 }
 
 function addAutoClicker() {
@@ -31,18 +30,35 @@ function addAutoClicker() {
     clickerCntr.innerText = donutMaker.clickerTally;
     console.log(donutMaker.donutTally);
   });
-  
-  
+  // possible spot for set interval
 }
 
 function addMultiplier() {
   const multiplyBtn = document.querySelector(".multiplyBtn");
   const autoBtn = document.querySelector(".autoBtn");
   const clickerCntr = document.querySelector("#clickerCntr");
-  
+
   multiplyBtn.addEventListener("click", () => {
     donutMaker.getMultiplier();
     donutTallyDisplay.innerText = donutMaker.donutTally;
-    multiplyCntr.innerText = donutMaker.numberOfMultipliers; 
-  })
+    multiplyCntr.innerText = donutMaker.numberOfMultipliers;
+  });
+}
+
+function activateAutoClicker() {
+  donutMaker.addADonut();
+  donutTallyDisplay.innerText = donutMaker.donutTally;
+}
+
+function addAutoClicker() {
+  const autoBtn = document.querySelector(".autoBtn");
+  const clickerCntr = document.querySelector("#clickerCntr");
+
+  autoBtn.addEventListener("click", () => {
+    donutMaker.buyAutoClicker();
+    donutTallyDisplay.innerText = donutMaker.donutTally;
+    clickerCntr.innerText = donutMaker.clickerTally;
+    console.log(donutMaker.donutTally);
+    setInterval(activateAutoClicker, 1000);
+  });
 }
